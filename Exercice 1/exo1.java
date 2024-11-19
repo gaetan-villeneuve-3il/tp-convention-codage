46a8996c11cb131127f926ab1e0e0264930e2e4b
Fantomas871 <87449324+Fantomas871@users.noreply.github.com>
2024-11-19 15:27:33 +0100

    commit 1

--- Exercice 1/exo1.java
+++ Exercice 1/exo1.java
@@ -1,6 +1,14 @@
-public class animal {
-public String nom;
-public int age;
-public void parler(){System.out.println("Je suis un animal.");}
-public void AfficherInfo(){System.out.println("Nom: "+nom+", Âge: "+age+" ans");}
+public class Animal {
+    protected String nom;
+    protected int age;
+
+    public void parler(){
+        System.out.println("Je suis un animal.");
+        }
+
+    public void afficherInfo(){
+        System.out.println("Nom: "+this.nom+", Âge: "+this.age+" ans");
+        }
 }
+
+//Cette classe défini un animal
--- Exercice 2/exo2.java
+++ Exercice 2/exo2.java
@@ -1,13 +1,34 @@
 public class Veterinaire {
+    
     public void soignerAnimal(String type, String nom) {
+        
+        Soins(type,nom);
+        Examens(type);
+        effectuer(type);
+        
+    }
+    
+    public void Soins(String type, String nom){
+        System.out.println("Soins pour le " + type + nom);
+    }
+    
+    public void Examens(String type){
         if (type.equals("Chien")) {
-            System.out.println("Soins pour le chien " + nom);
             System.out.println("Examen des pattes");
+        } else if (type.equals("Chat")) {
+            System.out.println("Examen des griffes");
+        }
+    }
+    
+    public void effectuer(String type){
+        if (type.equals("Chien")) {
             System.out.println("Injection de vaccin");
         } else if (type.equals("Chat")) {
-            System.out.println("Soins pour le chat " + nom);
-            System.out.println("Examen des griffes");
             System.out.println("Nettoyage des oreilles");
         }
     }
+    
+    
 }
+
+//Cette classe décrit un vétérinaire
